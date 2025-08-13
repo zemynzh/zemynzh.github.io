7d15796feeb0625aa96eddfb0578e54c607cd159
zemyn <zemynzh@gmail.com>
2025-08-13 19:23:38 +0800

    Update workflow to build Intlayer config before main build

--- .intlayer/dynamic_dictionary/app.cjs
+++ .intlayer/dynamic_dictionary/app.cjs
@@ -1,6 +1,6 @@
 const content = {
-  'zh-Hans': () => Promise.resolve(require('./app.zh-Hans.json.cjs')),
-  'en': () => Promise.resolve(require('./app.en.json.cjs')),
-  'ja': () => Promise.resolve(require('./app.ja.json.cjs'))
+  'zh-Hans': () => Promise.resolve(require('./app.zh-Hans.json')),
+  'en': () => Promise.resolve(require('./app.en.json')),
+  'ja': () => Promise.resolve(require('./app.ja.json'))
 };
 module.exports = content;
